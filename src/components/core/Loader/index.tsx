661f03bcb5dbdc2fec87fdf5ee9461b1423413b1
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-12-30 10:52:43 -0300

    feat: loader component support a custom colors

--- src/components/core/Loader/index.tsx
+++ src/components/core/Loader/index.tsx
@@ -3,19 +3,25 @@ import React from 'react';
 import styles from './style.module.css';
 
 interface InterfaceLoader {
-  size: number;
+  color?: string;
   loading?: boolean;
+  transparentColor?: string;
+  size: number;
 };
 
-const Loader = ({ size, loading }: InterfaceLoader) => (
+const Loader = ({ color = '#000000', loading, transparentColor = '#FFFFFF', size }: InterfaceLoader) => (
   <div className={`${styles.loader} ${loading && styles.animation}`} style={{
-    width: size,
+    backgroundColor: color,
     height: size,
+    width: size,
   }}>
     <div className={styles.horizontalDivisor} style={{
+      backgroundColor: transparentColor,
       height: size/10,
     }}></div>
     <div className={styles.centerBall} style={{
+      backgroundColor: color,
+      borderColor: transparentColor,
       borderWidth: size/10,
       height: size/2,
       width: size/2,
