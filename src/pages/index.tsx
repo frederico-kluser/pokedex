81658ab3477247fc7fae1f5c0328f9098166f4d8
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-12-30 17:32:34 -0300

    refactor: change homepage structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,13 @@ import PageContainer from 'src/components/template/PageContainer'
 import useFetch from 'src/hooks/useFetch'
 import { TypePokemon } from 'src/types/pokemon';
 import Header from 'src/components/core/Header';
+import Input from 'src/components/core/Input';
+import getFilteredArray from 'src/components/functions/filter';
 
 export default function Home() {
   const { data, loading, error } = useFetch<{pokemon: TypePokemon[]}>('https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json');
   const [pokemons, setPokemons] = useState<TypePokemon[]>([]);
+  const [filter, setFilter] = useState<string>('');
 
   useEffect(() => {
     if (data) {
@@ -19,8 +22,10 @@ export default function Home() {
     <PageContainer title="Pokedex" loader={loading}>
       {!error && (
         <>
-          <Header />
-          <PokemonList pokemons={pokemons} />
+          <Header>
+            <Input placeholder='Search pokemon name' state={filter} setState={setFilter} />
+          </Header>
+          <PokemonList pokemons={getFilteredArray(pokemons, filter)} />
         </>
       )}
       {error && <h1>{error}</h1>}
