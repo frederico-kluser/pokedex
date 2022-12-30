da50fc1869083e916ecfa0b5ef1fa93ee6f3e6d5
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-12-30 08:44:39 -0300

    feat: improve page container component

--- src/components/template/PageContainer/index.tsx
+++ src/components/template/PageContainer/index.tsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+
 import styles from './style.module.css';
 
-const PageContainer = () => <div className={styles.container}></div>
+interface InterfacePageContainer {
+  children: ReactNode;
+}
+
+const PageContainer = (props: InterfacePageContainer) => (<div className={styles.container}>{props.children}</div>)
 
 export default PageContainer;
