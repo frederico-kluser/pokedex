9de8f58c9933fca42d8bdabbc41aa2b5f443f035
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2022-12-30 18:17:54 -0300

    test: create input component test

---
+++ src/components/core/Input/index.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react"
+import Input from "."
+
+describe('Input component', () => {
+  const placeholder = 'test placeholder';
+  const value = "test";
+
+  beforeEach(() => {
+    render(<Input placeholder={placeholder} state={value} setState={() => {}}  />);
+  });
+
+  it('render input component', () => {
+    // ACT
+    const inputNode = screen.getByPlaceholderText(placeholder);
+  
+    // ASSERT
+    expect(inputNode).toBeInTheDocument();
+  });
+
+  it('render input component', () => {
+    // ACT
+    const inputNode: HTMLInputElement = screen.getByPlaceholderText(placeholder);
+    
+    // ASSERT
+    expect(inputNode.value).toBe(value);
+  });
+});
