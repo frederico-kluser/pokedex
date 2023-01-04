88d7c0505351a0ec5f9d0e771f445c5eb2b19d65
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-01-04 13:03:01 -0300

    feat: change background color based in theme context

--- src/components/template/PageContainer/index.tsx
+++ src/components/template/PageContainer/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useContext } from 'react';
+import { ThemeContext } from '../../../context/theme';
 import Loader from '../../core/Loader';
 
 import styles from './style.module.css';
@@ -10,19 +11,27 @@ interface InterfacePageContainer {
   title: string;
 };
 
-const PageContainer = ({ children, loader, title }: InterfacePageContainer) => (<div className={styles.container}>
-  <Head>
-    <title>{ title }</title>
-    <link rel="icon" href="/favicon.ico" />
-  </Head>
-  <main className={styles.content}>
-    {loader && (
-      <div className={styles.loaderContainer}>
-        <Loader size={100} loading />
-      </div>
-    )}
-    {!loader && children}
-  </main>
-</div>);
+const PageContainer = ({ children, loader, title }: InterfacePageContainer) => {
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <div className={styles.container} style={{
+      backgroundColor: theme === 'light' ? '#FFFFFF' : '#212121',
+    }}>
+      <Head>
+        <title>{ title }</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.content}>
+        {loader && (
+          <div className={styles.loaderContainer}>
+            <Loader size={100} loading />
+          </div>
+        )}
+        {!loader && children}
+      </main>
+    </div>
+  );
+};
 
 export default PageContainer;
