ec15354a8a56db618c2e2116e7bd34bda85946fd
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2023-01-04 12:52:57 -0300

    refactor: improve use fetch hook type structure

--- src/components/ui/PokemonList/index.tsx
+++ src/components/ui/PokemonList/index.tsx
@@ -8,10 +8,12 @@ interface InterfacePokemonList {
   pokemons: TypePokemon[];
 };
 
-const PokemonList = ({ pokemons = [] }: InterfacePokemonList) => (
+const PokemonList = ({ pokemons = [] }: InterfacePokemonList) => {
+  console.log('renderizou pokemonList');
+  return (
   <div className={styles.container}>
     {pokemons.map(({ img, name, num, type }) => <PokemonCard key={num} img={img} name={name} num={num} type={type} />)}
   </div>
-);
+)};
 
 export default PokemonList;
